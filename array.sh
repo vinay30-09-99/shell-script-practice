a7ab7553c97dd351c3b553ac1d99780c4deb4e90
vinay <vinaykocherla123@gmail.com>
2025-07-26 07:55:41 -0700

    shell-files started

--- array.sh
+++ array.sh
@@ -4,4 +4,8 @@
 
 BOOKS=("SCIENTIFIC" "DRAMA" "LOVE STORY" "ROMANTIC" "HORROR" "THRILLER")
 
-echo "TRACK1 is: ${BOOKS[0]}"
+echo "TRACK1 is: ${BOOKS[0]}"   #--- i need first book
+
+#----- i need all the books
+
+echo "ALL TRACKS are: ${BOOKS[%]}"
