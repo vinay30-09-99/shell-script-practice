bdd4061bc7cc89c712e25dfac9b7f6dbd0e71346
vinay <vinaykocherla123@gmail.com>
2025-07-26 07:02:29 -0700

    shell-files started

---
+++ 01variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#------ variables using arguments
+
+PER1=$1
+PER2=$2
+
+echo "$PER1:: hi, how are you ?"
+echo "$PER2:: hi fine you ?"
+echo "$PER1:: good, can we go out?" 
+echo "$PER2:: yeah sure, $PER1"
