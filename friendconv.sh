e9767dc4c4803ac79b21cbe249ad3f7aeebbb35c
vinay <vinaykocherla123@gmail.com>
2025-07-26 06:41:02 -0700

    shell-files started

--- friendconv.sh
+++ friendconv.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-PER1= bhargav
-PER2= vinay
-
-echo "$PER1:: hi, how are you ?"
-echo "$PER2:: hi fine you ?"
-echo "$PER1:: good, can we go out?"
-echo "$PER2:: yeah sure, will go"
+echo "bhargav:: hi, how are you ?"
+echo "vinay:: hi fine you ?"
+echo "bhargav:: good, can we go out?"
+echo "vinay:: yeah sure, bhargav"
