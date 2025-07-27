995b03583741802d199191fb6a4fe550477d19ff
vinay <vinaykocherla123@gmail.com>
2025-07-27 18:21:50 +0530

    shell-files started

--- install-mysql.sh
+++ install-mysql.sh
@@ -7,7 +7,15 @@ then
     echo "ERROR:: please run with root access" 
     exit 1       #--- here, it execute manually (other than 0 -- 1 to 127)  --- 0 -- success exit status, 1to 127 -- failure exit status
 else
-    echo "already in  root access"
+    echo "now you are in root access"
 fi
 
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then 
+    echo "Installing MYSQL-----SUCCESS"
+else
+    echo "Installing MYSQL-----FAILURE"
+fi
+
