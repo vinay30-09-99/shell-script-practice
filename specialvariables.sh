622195cc24ab3de94d69f334269811a8e9eea0af
vinay <vinaykocherla123@gmail.com>
2025-07-27 00:25:16 -0700

    shell-files started

---
+++ specialvariables.sh
@@ -0,0 +1,13 @@
+NAME1=$1
+NAME2=$2
+
+echo "My name is $NAME1"
+echo "My name is $NAME2"
+echo "$NAME1 and $NAME2 are friends"
+
+FOOD=("INDIAN" "ITALIAN" "CHINESE" "FRANCE")
+
+echo "i need all types of:${FOOD[@]}"
+echo "how many types you have:${FOOD[#]}"
+
+echo "my script name is: $0"
