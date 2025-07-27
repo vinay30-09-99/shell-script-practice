137868b1afb624206a395b9ad034d5a08e09628f
vinay <vinaykocherla123@gmail.com>
2025-07-27 18:16:45 +0530

    shell-files started

--- install-mysql.sh
+++ install-mysql.sh
@@ -2,9 +2,10 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]            
 then
     echo "ERROR:: please run with root access" 
+    exit 1       #--- here, it execute manually (other than 0 -- 1 to 127)  --- 0 -- success exit status, 1to 127 -- failure exit status
 else
     echo "already in  root access"
 fi
