a0ad294d2553081ed6843723915a17c0b55ddd27
vinay <vinaykocherla123@gmail.com>
2025-07-29 11:12:48 +0530

    shell-files started

--- colors.sh
+++ colors.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo -e "\e[34m DO PRACTICE...."
+echo -e "\e[34m DO PRACTICE...."    
+
+echo "perfection is important"
+
+#here, above every line getting same color
+
+#but, i need 1st line only color, next line no color
+
+echo -e " \e[36m   DO PRACTICE....   \e[0m "        #--- i am using this \e[0m , it will stop coloring
 
 echo "perfection is important"
