17131432e54d11a6e86cc51526125758d4d560e7
vinay <vinaykocherla123@gmail.com>
2025-07-29 11:15:51 +0530

    shell-files started

--- colors.sh
+++ colors.sh
@@ -5,9 +5,8 @@ echo -e "\e[34m DO PRACTICE...."
 echo "perfection is important"
 
 #here, above every line getting same color
-
 #but, i need 1st line only color, next line no color
 
-echo -e " \e[36m  DO PRACTICE....\e[0m "        #--- i am using this \e[0m , it will stop coloring
+echo -e "\e[36m  DO PRACTICE....\e[0m"        #--- i am using this \e[0m , it will stop coloring
 
-echo "perfection is important"
+echo "perfection is important1"
