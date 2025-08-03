8c1b69df769a44dae492e7f081af27d0c7ddf141
vinay <vinaykocherla123@gmail.com>
2025-08-03 09:40:00 +0530

    shell-files started

---
+++ fun-logs.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#here, i am adding colors
+R="\e[31m"  #31--RED
+G="\e[32m"  #32--GREEN
+M="\e[35m"  #35--MAGENTA
+N="\e[0m"   #means, no color
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executing at: $(date)" &>>$LOG_FILE
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: $N please run with root access" &>>$LOG_FILE
+    exit 1
+else
+    echo -e "$G now you are in root acess $N" &>>$LOG_FILE
+fi
+
+#-- above code is for checking root access is there or not
+#-- function takes the repeated things
+#-- here, the below commenting steps are repeating
+#-- i will repeated as one
+
+#if [ $? -eq 0 ]
+#then
+#    echo "Installing MYSQL.... SUCCESS"
+#else
+#    echo "Installing MYSQL.... FAILURE"
+#fi
+
+VALIDATE()          #-- this is function(it is repeated)
+{
+    if [ $1 -eq 0 ]       #-- why means $1 -eq 0 (0==0)--> 
+    #install $2 
+    #(inplace of $2 -- mysql, pyhotn3,nginx,...)
+    then
+        echo -e "Installing $2....$G SUCCESS $N" &>>$LOG_FILE
+    else
+        echo -e "Installing $2....$R FAILURE $N" &>>$LOG_FILE
+    fi
+} 
+
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]          
+then
+    echo "MYSQL is not installed... need to install it" &>>$LOG_FILE
+    dnf install mysql -y
+    VALIDATE $? "mysql"        #-- validate $?=0 --> install MYSQL.... SUCCESS
+else
+    echo -e "$M MYSQL is already installed..... $N" &>>$LOG_FILE
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... need to install it" &>>$LOG_FILE
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else  
+    echo -e "$M python3 already installed..... $N" &>>$LOG_FILE
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]
+then 
+    echo "nginx is not installed... need to install it" &>>$LOG_FILE
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "$M nginx already installed..... $N" &>>$LOG_FILE
+fi
